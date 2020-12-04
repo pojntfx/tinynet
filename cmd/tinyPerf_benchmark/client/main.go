c5dc3a3ff24fa3abf6c8e825f51f1286e99cec77
Jakob Waibel <jakob.waibel@web.de>
2020-12-04 17:06:56 +0100

    feat: Add flags to client

--- cmd/tinyPerf_benchmark/client/main.go
+++ cmd/tinyPerf_benchmark/client/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
 )
 
 func main() {
-	input := make([]byte, 100000)
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("0.0.0.0:8888"))
+	port := flag.String("p", "8888", "port to connect/listen to")
+	length := flag.Int("l", 128, "length of the buffers to send in Kb")
+	ip := flag.String("ip", "0.0.0.0", "ip to connect to")
+
+	*length = *length * 1000
+
+	input := make([]byte, *length)
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%v:%v", *ip, *port))
 	checkError(err)
 
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
